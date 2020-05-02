3938d209e494496f9e493c5481a985660a4ad8b9
felipee96 <afgb1996@gmail.com>
2020-05-02 16:50:10 -0500

    se crean migraciones

---
+++ database/migrations/2020_05_02_212237_create_registro_estudiante.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistroEstudiante extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registro_estudiante', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo');
+            $table->string('nombre',50);            
+            $table->string('apellido',50);
+            $table->string('facultad',50);
+            $table->string('programa',50);
+            $table->string('semestre',50);
+            $table->string('computador',50);
+            $table->string('monitor',50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registro_estudiante');
+    }
+}
---
+++ database/migrations/2020_05_02_214335_create_objperdido.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateObjperdido extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objperdido', function (Blueprint $table) {
+            $table->id();
+            $table->string('monitor',50);
+            $table->string('sede',50);
+            $table->string('sala',50);
+            $table->string('equipo',50);
+            $table->string('objeto',50);
+            $table->string('descripcion',200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('objperdido');
+    }
+}
